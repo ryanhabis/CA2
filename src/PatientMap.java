3bad16451593f5fbb418186abf35dde6f194df80
ryanhabis <ryan.habiss@gmail.com>
2023-05-02 11:36:45 +0100

    added PatientMap

--- src/PatientMap.java
+++ src/PatientMap.java
@@ -1,3 +1,16 @@
+import java.util.LinkedList;
+
 public class PatientMap
 {
+    // Declare instance variables
+    private final int mapSize = 100; // Assuming Map size is 100
+    private LinkedList<Patient>[] map;
+
+    public PatientMap() {
+        map = new LinkedList[mapSize];
+        for (int i = 0; i < mapSize; i++) {
+            map[i] = new LinkedList<Patient>();
+        }
+    }
+
 }
