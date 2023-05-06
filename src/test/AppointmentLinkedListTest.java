9853c59b52d142aea409efb5b9831d52713d0d0a
cgarvey00 <conorgarvey19@outlook.com>
2023-05-06 19:40:49 +0100

    Adding the AppointmentLinkedListTest Class

---
+++ src/test/AppointmentLinkedListTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDate;
+import java.util.NoSuchElementException;
+
+import utils.Patient;
+import org.junit.jupiter.api.Test;
+/**
+ *
+ * @author Conor Garvey
+ */
+public class AppointmentLinkedListTest {
+
+
+    public AppointmentLinkedListTest() {
+    }
+
+}
