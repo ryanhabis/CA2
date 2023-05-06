f53c973ad5d8335129e6d25496074b45c3a560f1
cgarvey00 <conorgarvey19@outlook.com>
2023-05-06 12:32:09 +0100

    Adding Patient constructor and instance variables

--- src/utils/Patient.java
+++ src/utils/Patient.java
@@ -1,4 +1,26 @@
 package utils;
 
+import java.time.LocalDate;
+import java.util.LinkedList;
+
 public class Patient {
+    private String firstName;
+    private String surName;
+    private LocalDate dob;
+    private LocalDate date;
+    private LinkedList appointment;
+
+    public Patient(String firstName, String surName, LocalDate dob, LocalDate date, LinkedList appointment) {
+        if (dob.compareTo(LocalDate.now()) > 0) {
+            throw new IllegalArgumentException("Date of Birth cannot be after today");
+        } else if (date.compareTo(LocalDate.now()) > 0) {
+            throw new IllegalArgumentException("Invalid Date,Patient SignUp date must be today or in the past");
+        }else {
+            this.firstName = firstName;
+            this.surName = surName;
+            this.dob = dob;
+            this.date = date;
+            this.appointment = appointment;
+        }
+    }
 }
