4d50f816596f549dcd0e03436e9a92bcae074425
cgarvey00 <conorgarvey19@outlook.com>
2023-05-06 15:10:09 +0100

    Adding Accessor and Mutator Methods

--- src/utils/Patient.java
+++ src/utils/Patient.java
@@ -21,6 +21,63 @@ public class Patient {
             this.dob = dob;
             this.date = date;
             this.appointment = appointment;
-        }
+        }//
     }
+    /**
+     * These are the Accessor Methods
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+    public String getSurName() {
+        return surName;
+    }
+
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+
+    public LinkedList getAppointment() {
+        return appointment;
+    }
+
+    /**
+     * These are the Mutator Methods
+     */
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public void setDob(LocalDate dob) {
+        if (dob.compareTo(LocalDate.now()) <= 0) {
+            throw new IllegalArgumentException("Date of Birth cannot be after today");
+        }
+        this.dob = dob;
+    }
+
+    public void setDate(LocalDate dob) {
+        if (dob.compareTo(LocalDate.now()) > 0) {
+            throw new IllegalArgumentException("Invalid Date,Patient SignUp date must be today or in the past");
+        }
+        this.dob = dob;
+    }
+
+    public void setAppointment(LinkedList appointment) {
+        this.appointment = appointment;
+    }
+
 }
