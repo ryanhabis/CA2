0ce132ef60e6aeb423a12c071a53dbeb56f0d997
cgarvey00 <conorgarvey19@outlook.com>
2023-05-07 14:52:23 +0100

    Adding the DuplicateAppointmentException to Exceptions class

--- src/DuplicateAppointmentException.java
+++
@@ -1,7 +0,0 @@
-public class DuplicateAppointmentException extends Exception {
-    public DuplicateAppointmentException() {
-        super("The Appointment has Already been booked");
-
-        }
-
-}
